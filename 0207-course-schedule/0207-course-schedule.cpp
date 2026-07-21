84559f79b7b44083cbef3fa787532017a08a5810
Indresh Thakur <ithakur2327@gmail.com>
2026-07-21 11:28:46 +0530

    Time: 3 ms (75.09%), Space: 19 MB (90.41%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,43 +1,30 @@
 class Solution {
 public:
-    bool dfs(vector<vector<int>>& adj, int node, vector<bool>& visited, vector<bool>& pathVisited) {
-        visited[node] = true;
-        pathVisited[node] = true;
-
-        for (int neighbour : adj[node]) {
-            if (!visited[neighbour]) {
-                if (dfs(adj, neighbour, visited, pathVisited)) {
-                    return true;
-                }
-            }
-            else if (pathVisited[neighbour]) {
-                return true;
-            }
-        }
-
-        pathVisited[node] = false;
-
-        return false;
-    }
-
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<vector<int>> adj(numCourses);
-
-        for (auto &edge : prerequisites) {
+    bool canFinish(int numCourses,vector<vector<int>>&prerequisites) {
+        vector<vector<int>>adj(numCourses);
+        vector<int>indegree(numCourses,0);
+        for(auto &edge:prerequisites) {
             adj[edge[1]].push_back(edge[0]);
+            indegree[edge[0]]++;
         }
-
-        vector<bool> visited(numCourses, false);
-        vector<bool> pathVisited(numCourses, false);
-
-        for (int i = 0; i < numCourses; i++) {
-            if (!visited[i]) {
-                if (dfs(adj, i, visited, pathVisited)) {
-                    return false;
+        queue<int>q;
+        for(int i=0;i<numCourses;i++) {
+            if(indegree[i]==0) {
+                q.push(i);
+            }
+        }
+        int count=0;
+        while(!q.empty()) {
+            int node=q.front();
+            q.pop();
+            count++;
+            for(int neighbour:adj[node]) {
+                indegree[neighbour]--;
+                if(indegree[neighbour]==0) {
+                    q.push(neighbour);
                 }
             }
         }
-
-        return true;
+        return count==numCourses;
     }
 };
