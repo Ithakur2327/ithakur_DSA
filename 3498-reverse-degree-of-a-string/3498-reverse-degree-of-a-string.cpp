58b19c350bc96dd446ab77283433ee9503055cfa
Indresh Thakur <ithakur2327@gmail.com>
2026-09-20 12:54:17 +0530

    Time: 0 ms (100%), Space: 9.7 MB (78.4%) - LeetHub

---
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int reverseDegree(string s) {
+        int sum=0;
+        for(int i=1;i<=s.size();i++){
+            int diff=123-s[i-1];
+            sum +=diff*i;
+        }
+        return sum;
+        
+    }
+};
